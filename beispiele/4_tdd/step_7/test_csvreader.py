b0b19eb76bf500d93f289ef34ba5c79b4784e44e
Christopher Arndt <chris@chrisarndt.de>
2025-05-27 16:36:35 +0200

    Improve test for cvsreader

--- beispiele/4_tdd/step_7/test_csvreader.py
+++ beispiele/4_tdd/step_7/test_csvreader.py
@@ -1,5 +1,7 @@
-def test_from_file(tmp_path):
-    from csvreader import CSVReader
+from dataclasses import dataclass
+from csvreader import CSVReader
+
+def test_from_file():
     with open("data.csv", "w") as csv:
         csv.write("a,b,c\n1,2,3\n")
     
@@ -7,3 +9,34 @@ def test_from_file(tmp_path):
         parser = CSVReader(csv)
         result = list(parser)
         assert result == [["a", "b", "c"], ["1", "2", "3"]]
+
+def test_from_file_semicolon():
+    with open("data.csv", "w") as csv:
+        csv.write("a;b;c\n1;2;3\n")
+    
+    with open("data.csv") as csv:
+        parser = CSVReader(csv, ";", tuple)
+        result = list(parser)
+        assert result == [("a", "b", "c"), ("1", "2", "3")]
+
+def test_from_file_dataclass():
+    @dataclass
+    class Row:
+        a: str
+        b: str
+        c: str
+    
+        @classmethod
+        def fromlist(cls, lst):
+            return cls(*lst)
+
+    with open("data.csv", "w") as csv:
+        csv.write("a;b;c\n1;2;3\n")
+
+    with open("data.csv") as csv:
+        parser = CSVReader(csv, ";", Row.fromlist)
+        result = list(parser)
+        assert isinstance(result[0], Row)
+        assert result[0].a == 'a'
+        assert result[0].b == 'b'
+        assert result[0].c == 'c'
